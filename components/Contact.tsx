b9970ca0abf1589cf0c6fb5751c37125e29f6d4c
Joshua Ozorji <ozorjijoshua@gmail.com>
2023-11-30 15:27:32 +0100

    clear form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import SectionHeading from "./SectionHeading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
@@ -9,6 +9,22 @@ import SubmitBtn from "./SubmitBtn";
 
 const Contact = () => {
 	const { ref } = useSectionInView("Contact");
+
+	// State to manage form inputs
+	const [formData, setFormData] = useState({
+		senderEmail: "",
+		message: "",
+	});
+
+	const handleInputChange = (
+		e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+	) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	};
+
 	return (
 		<section ref={ref} id='contact' className='scroll-mt-28 mb-28 padding'>
 			<SectionHeading>
@@ -41,8 +57,15 @@ const Contact = () => {
 				transition={{ delay: 0.75 }}
 				viewport={{ once: true }}
 				className='mt-10 flex flex-col justify-start font-abel .sm-md-paragraph-text'
-				action={async (formData) => {
-					const { data, error } = await sendEmail(formData);
+				onSubmit={async (e) => {
+					e.preventDefault();
+
+					// Create a FormData object
+					const formDataObject = new FormData();
+					formDataObject.append("senderEmail", formData.senderEmail);
+					formDataObject.append("message", formData.message);
+
+					const { data, error } = await sendEmail(formDataObject);
 
 					if (error) {
 						toast.error(error);
@@ -50,6 +73,12 @@ const Contact = () => {
 					}
 
 					toast.success("Email sent successfully!");
+
+					// Reset form inputs after successful email send
+					setFormData({
+						senderEmail: "",
+						message: "",
+					});
 				}}>
 				<input
 					className='h-12 rounded-md border border-black/10 dark:border-white/10 transition-all duration-300 p-4 dark:bg-opacity-50 form-focus'
@@ -57,6 +86,8 @@ const Contact = () => {
 					type='email'
 					required
 					max={100}
+					value={formData.senderEmail}
+					onChange={handleInputChange}
 					placeholder='Your email'
 				/>
 				<textarea
@@ -65,6 +96,8 @@ const Contact = () => {
 					placeholder='Your message'
 					required
 					maxLength={1000}
+					value={formData.message}
+					onChange={handleInputChange}
 				/>
 				<span className='inline-block'>
 					<SubmitBtn />
